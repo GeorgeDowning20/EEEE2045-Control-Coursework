1e82c886b0333eca5f05da688d5c351cb53d54d5
George Downing <george.downing20@icloud.com>
2022-10-24 23:51:09 +0100

    exercise 2 complete

---
+++ matlab/Ex2.m
@@ -0,0 +1,25 @@
+
+figure;
+hold on;
+
+Gp=tf([1],[1 1])
+step(Gp,'b')
+
+Gp=tf([3],[1 3])
+step(Gp,'r')
+
+Gp=tf([12],[1 12])
+step(Gp,'g')
+
+Gp=tf([20],[1 20])
+step(Gp,'k')
+
+cleanfigure; 
+matlab2tikz('Ex2.tex');
+
+figure;
+Gp=tf([20],[1 -20])
+step(Gp)
+axis([0 1 0 500000]);
+cleanfigure
+matlab2tikz('Ex2_2.tex'); 
